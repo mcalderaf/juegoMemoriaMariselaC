863b934e4086c4cf478274c6f90a18b171a01375
Marisela Caldera <mariselacaldera@MacBook-Pro-de-Marisela.local>
2016-06-24 12:15:07 -0600

    first commit

---
+++ JuegoMemoria.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+for i in 0...100{
+    if i%5 == 0 {
+        print("\(i) Bingo¡¡¡")
+    }
+    if i%2 == 0 {
+        print("\(i) Par¡¡¡")
+    } else {
+        print("\(i) Impar¡¡¡")
+    }
+    if i>=30 && i<=40 {
+        print("\(i) Viva Swift¡¡¡")
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # juegoMemoriaMariselaC
+# juegoMemoria
